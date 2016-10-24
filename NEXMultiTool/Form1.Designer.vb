7bec8a1f9ade2ee9c3b2a1b502a5ab40573e7765
krminix <krminix@gmail.com>
2016-10-24 18:33:53 -0400

    First 2 Printers added, File menu WIP.

--- NEXMultiTool/Form1.Designer.vb
+++ NEXMultiTool/Form1.Designer.vb
@@ -37,13 +37,21 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.toolStripSeparator5 = New System.Windows.Forms.ToolStripSeparator()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(13, 13)
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 34)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(146, 17)
         Me.CheckBox1.TabIndex = 0
@@ -53,17 +61,17 @@ Partial Class Form1
         'CheckBox2
         '
         Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(13, 36)
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 57)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox2.Size = New System.Drawing.Size(121, 17)
         Me.CheckBox2.TabIndex = 1
-        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.Text = "NEX1 Canon Plotter"
         Me.CheckBox2.UseVisualStyleBackColor = True
         '
         'CheckBox3
         '
         Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Location = New System.Drawing.Point(13, 59)
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 80)
         Me.CheckBox3.Name = "CheckBox3"
         Me.CheckBox3.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox3.TabIndex = 3
@@ -73,7 +81,7 @@ Partial Class Form1
         'CheckBox4
         '
         Me.CheckBox4.AutoSize = True
-        Me.CheckBox4.Location = New System.Drawing.Point(13, 82)
+        Me.CheckBox4.Location = New System.Drawing.Point(12, 103)
         Me.CheckBox4.Name = "CheckBox4"
         Me.CheckBox4.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox4.TabIndex = 2
@@ -83,7 +91,7 @@ Partial Class Form1
         'CheckBox5
         '
         Me.CheckBox5.AutoSize = True
-        Me.CheckBox5.Location = New System.Drawing.Point(13, 105)
+        Me.CheckBox5.Location = New System.Drawing.Point(12, 126)
         Me.CheckBox5.Name = "CheckBox5"
         Me.CheckBox5.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox5.TabIndex = 4
@@ -93,7 +101,7 @@ Partial Class Form1
         'CheckBox6
         '
         Me.CheckBox6.AutoSize = True
-        Me.CheckBox6.Location = New System.Drawing.Point(13, 128)
+        Me.CheckBox6.Location = New System.Drawing.Point(12, 149)
         Me.CheckBox6.Name = "CheckBox6"
         Me.CheckBox6.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox6.TabIndex = 5
@@ -103,7 +111,7 @@ Partial Class Form1
         'CheckBox7
         '
         Me.CheckBox7.AutoSize = True
-        Me.CheckBox7.Location = New System.Drawing.Point(13, 151)
+        Me.CheckBox7.Location = New System.Drawing.Point(12, 172)
         Me.CheckBox7.Name = "CheckBox7"
         Me.CheckBox7.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox7.TabIndex = 6
@@ -113,7 +121,7 @@ Partial Class Form1
         'CheckBox8
         '
         Me.CheckBox8.AutoSize = True
-        Me.CheckBox8.Location = New System.Drawing.Point(13, 174)
+        Me.CheckBox8.Location = New System.Drawing.Point(12, 195)
         Me.CheckBox8.Name = "CheckBox8"
         Me.CheckBox8.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox8.TabIndex = 7
@@ -123,7 +131,7 @@ Partial Class Form1
         'CheckBox9
         '
         Me.CheckBox9.AutoSize = True
-        Me.CheckBox9.Location = New System.Drawing.Point(13, 197)
+        Me.CheckBox9.Location = New System.Drawing.Point(12, 218)
         Me.CheckBox9.Name = "CheckBox9"
         Me.CheckBox9.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox9.TabIndex = 8
@@ -133,7 +141,7 @@ Partial Class Form1
         'CheckBox10
         '
         Me.CheckBox10.AutoSize = True
-        Me.CheckBox10.Location = New System.Drawing.Point(13, 220)
+        Me.CheckBox10.Location = New System.Drawing.Point(12, 241)
         Me.CheckBox10.Name = "CheckBox10"
         Me.CheckBox10.Size = New System.Drawing.Size(87, 17)
         Me.CheckBox10.TabIndex = 9
@@ -142,7 +150,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(122, 227)
+        Me.Button1.Location = New System.Drawing.Point(84, 267)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 10
@@ -151,7 +159,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(206, 208)
+        Me.Button2.Location = New System.Drawing.Point(206, 241)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(66, 23)
         Me.Button2.TabIndex = 11
@@ -161,7 +169,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(203, 240)
+        Me.Label1.Location = New System.Drawing.Point(203, 267)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(69, 13)
         Me.Label1.TabIndex = 12
@@ -170,18 +178,68 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.NEXMultiTool.My.Resources.Resources.NEX_LOGO_2015_small
-        Me.PictureBox1.Location = New System.Drawing.Point(172, 13)
+        Me.PictureBox1.Location = New System.Drawing.Point(171, 34)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 13
         Me.PictureBox1.TabStop = False
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(169, 280)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Created by Keith Minix"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 15
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.toolStripSeparator5, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'toolStripSeparator5
+        '
+        Me.toolStripSeparator5.Name = "toolStripSeparator5"
+        Me.toolStripSeparator5.Size = New System.Drawing.Size(149, 6)
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "&About..."
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(284, 302)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
@@ -196,10 +254,14 @@ Partial Class Form1
         Me.Controls.Add(Me.CheckBox4)
         Me.Controls.Add(Me.CheckBox2)
         Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "NEXMultiTool"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -218,5 +280,12 @@ Partial Class Form1
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents toolStripSeparator5 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 
 End Class
--- NEXMultiTool/Form1.vb
+++ NEXMultiTool/Form1.vb
@@ -1,3 +1,50 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If CheckBox1.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("rundll32 printui.dll PrintUIEntry /in /n \\nexfileserverb\nex1_FrtOffice_Canon_C5235")
+        End If
+        If CheckBox2.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("rundll32 printui.dll PrintUIEntry /in /n \\nexfileserverb\nex1_FrtOffice_Canon_Plotter")
+        End If
+        If CheckBox3.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox4.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox5.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox6.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox7.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox8.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox9.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        If CheckBox10.Checked = True Then
+            Dim OpenCMD = CreateObject("wscript.shell")
+            OpenCMD.run("")
+        End If
+        MsgBox("Installation is complete!")
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
 End Class
